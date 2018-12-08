2e2cb49a64dca1dfe4f3d5dcd5dae0204286e21f
ice1x <ice1x@gmail.com>
2018-12-08 19:30:22 +0500

    Fix json.dump writing

--- pcleaner.py
+++ pcleaner.py
@@ -3,6 +3,10 @@ import json
 import hashlib
 
 
+TARGED_DIRECTORY = "/Users/cold00n/SAMPLES/_projects/"
+FILENAME = "./pcleaner_outfule.json"
+
+
 def hashfile(path, blocksize=65536):
     afile = open(path, 'rb')
     hasher = hashlib.md5()
@@ -14,46 +18,53 @@ def hashfile(path, blocksize=65536):
     return hasher.hexdigest()
 
 
-def getsubs(dir):
+def getsubs(path):
     # get all
 
-    dirs = []
-    # files = []
+    paths = []
     index = {}
     diff = {}
 
-    def update_index(filepath, hash, size):
-        if hash in index:
-            index[hash].append([size, filepath])
-            diff.update({hash: index[hash]})
+    def update_index(file_path, file_hash, size):
+        """
+        Update dictionary 'index' by:
+        KEY: :param file_hash:
+        and
+        VALUE: [:param size:, :param file_path:]
+        """
+        if file_hash in index:
+            # Update value for existent key
+            index[file_hash].append([size, file_path])
+            diff.update({file_hash: index[file_hash]})
         else:
-            index.update({hash: [[size, filepath]]})
+            # Add new key
+            index.update({file_hash: [[size, file_path]]})
 
-    for dirname, dirnames, filenames in os.walk(dir):
+    for path_name, path_names, file_names in os.walk(path):
 
-        dirs.append(dirname)
+        paths.append(path_name)
 
-        for subdirname in dirnames:
-            dirs.append(os.path.join(dirname, subdirname))
+        for sub_path_name in path_names:
+            paths.append(os.path.join(path_name, sub_path_name))
 
-        for filename in filenames:
-            # files.append(os.path.join(dirname, filename))
-            fullpath = os.path.join(dirname, filename)
+        for file_name in file_names:
+            full_path = os.path.join(path_name, file_name)
             try:
-                size = os.path.getsize(fullpath)
+                size = os.path.getsize(full_path)
             except OSError:
                 pass
             else:
-                update_index(fullpath, hashfile(fullpath), size)
+                update_index(full_path, hashfile(full_path), size)
 
     return diff
 
-d = getsubs("/Users/cold00n/SAMPLES/")
+
+data = getsubs(TARGED_DIRECTORY)
+with open(FILENAME, 'w') as outfile:
+    json.dump(data, outfile, ensure_ascii=False)
 
 """
-with open(filename, 'wb') as outfile:
-    json.dump(data, outfile)
-with open(filename) as infile:
+with open(FILENAME) as infile:
     data = json.load(infile)
 """
 
